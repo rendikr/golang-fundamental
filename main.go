a435638872f6b359aefb78b7fa379ee1c5662c9a
Rendi K <rendi.krinaldi@gmail.com>
2021-03-06 14:15:23 +0700

    pointer

--- main.go
+++ main.go
@@ -2,39 +2,15 @@ package main
 
 import (
 	"fmt"
-	"golang-fundamental/management"
 )
 
 func main() {
-	firstUser := management.User{}
-	firstUser.ID = 1
-	firstUser.FirstName = "Rendi"
-	firstUser.LastName = "Kurnia"
-	firstUser.Username = "rendikr"
-	firstUser.IsActive = true
+	numberA := 5
+	numberB := &numberA
+	numberC := *numberB
 
-	displayUser := firstUser.Display()
-	fmt.Println(displayUser)
+	fmt.Println(numberA)
+	fmt.Println(numberB)
+	fmt.Println(numberC)
 
-	secondUser := management.User{
-		ID: 2,
-		FirstName: "Asep",
-		LastName: "Jaelani",
-		Username: "ajae",
-		IsActive: true,
-	}
-
-	thirdUser := management.User{
-		ID: 3,
-		FirstName: "Maman",
-		LastName: "Jawa",
-		Username: "Mamawa",
-		IsActive: true,
-	}
-
-	users := []management.User{firstUser, secondUser, thirdUser}
-
-	group := management.Group{"Gamer", secondUser, users, true}
-
-	group.Display()
 }
