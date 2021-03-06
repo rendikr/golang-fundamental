2672b132e62c619df0adc35ffc245b170392bc71
Rendi K <rendi.krinaldi@gmail.com>
2021-03-06 11:59:16 +0700

    slice

--- main.go
+++ main.go
@@ -1,47 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	var languages [5]string
+	var gamingConsoles []string
 
-	languages[0] = "Go"
-	languages[1] = "PHP"
-	languages[2] = "NodeJS"
-	languages[3] = "ReactJS"
-	languages[4] = "VueJS"
+	gamingConsoles = append(gamingConsoles, "Playstation 4")
+	gamingConsoles = append(gamingConsoles, "Playstation 5")
+	gamingConsoles = append(gamingConsoles, "Nintendo Switch")
 
-	length := len(languages)
-	fmt.Println(languages)
-	fmt.Println(length)
+	fmt.Println(gamingConsoles)
 
-	ages := [3]int{4, 2, 31}
-
-	ageLength := len(ages)
-	fmt.Println(ages)
-	fmt.Println(ageLength)
-
-	topics := [3]string{
-		"General",
-		"Finance",
-		"Customer Service",
-	}
-
-	topicsLength := len(topics)
-	fmt.Println(topics)
-	fmt.Println(topicsLength)
-
-	engineers := [...]string{
-		"Frontend Engineer",
-		"Backend Engineer",
-		"DevOps Engineer",
-	}
-
-	engineersLength := len(engineers)
-	fmt.Println(engineers)
-	fmt.Println(engineersLength)
-
-	for index, engineer := range engineers {
-		fmt.Println("Index :", index, " Engineer :", engineer)
+	for _, console := range gamingConsoles {
+		fmt.Println(console)
 	}
 }
