111671d7fc14ab9bc41189cb4ec0a6a5d699cd93
Rendi K <rendi.krinaldi@gmail.com>
2021-03-06 11:40:03 +0700

    quiz

--- main.go
+++ main.go
@@ -3,23 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	for i := 1; i <= 10; i++ {
-		fmt.Println("I'm learning Go! :", i)
+	sentence := "Golang the best language"
+
+	for index, letter := range sentence {
+		if index % 2 == 0 {
+			fmt.Println("Index :", index, " Letter :", string(letter))
+		}
 	}
 
-	n := 11
-	for n <= 20 {
-		fmt.Println("I'm learning Go! :", n)
-		n++
-	}
+	fmt.Println("---")
 
-	title := "Golang Programming Language"
-	for index, letter := range title {
-		fmt.Println("Index :", index, " Letter :", letter, " Letter String: ", string(letter))
-	}
-
-	newTitle := "Golang Best For Learning"
-	for _, letter := range newTitle {
-		fmt.Println("Letter :", letter, " Letter String: ", string(letter))
+	for index, letter := range sentence {
+		switch string(letter) {
+			case "a", "i", "u", "e", "o":
+				fmt.Println("Index :", index, " Letter :", string(letter))
+		}
 	}
 }
