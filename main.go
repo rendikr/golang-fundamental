69f83ea5e0501e3ba1f6c41e7135841fa1efe5e9
Rendi K <rendi.krinaldi@gmail.com>
2021-03-06 11:52:26 +0700

    array

--- main.go
+++ main.go
@@ -3,20 +3,45 @@ package main
 import "fmt"
 
 func main() {
-	sentence := "Golang the best language"
+	var languages [5]string
 
-	for index, letter := range sentence {
-		if index % 2 == 0 {
-			fmt.Println("Index :", index, " Letter :", string(letter))
-		}
+	languages[0] = "Go"
+	languages[1] = "PHP"
+	languages[2] = "NodeJS"
+	languages[3] = "ReactJS"
+	languages[4] = "VueJS"
+
+	length := len(languages)
+	fmt.Println(languages)
+	fmt.Println(length)
+
+	ages := [3]int{4, 2, 31}
+
+	ageLength := len(ages)
+	fmt.Println(ages)
+	fmt.Println(ageLength)
+
+	topics := [3]string{
+		"General",
+		"Finance",
+		"Customer Service",
 	}
 
-	fmt.Println("---")
+	topicsLength := len(topics)
+	fmt.Println(topics)
+	fmt.Println(topicsLength)
 
-	for index, letter := range sentence {
-		switch string(letter) {
-			case "a", "i", "u", "e", "o":
-				fmt.Println("Index :", index, " Letter :", string(letter))
-		}
+	engineers := [...]string{
+		"Frontend Engineer",
+		"Backend Engineer",
+		"DevOps Engineer",
+	}
+
+	engineersLength := len(engineers)
+	fmt.Println(engineers)
+	fmt.Println(engineersLength)
+
+	for index, engineer := range engineers {
+		fmt.Println("Index :", index, " Engineer :", engineer)
 	}
 }
